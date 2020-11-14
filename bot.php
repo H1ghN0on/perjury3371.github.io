3e809473d26a32c292a6b411d8ad02078d7b2e4e
perjury3371 <65870074+perjury3371@users.noreply.github.com>
2020-11-15 00:46:17 +0300

    Update bot.php

--- bot.php
+++ bot.php
@@ -3,7 +3,7 @@
 include "vk_api.php"; 
 
 
-const VK_KEY = "3a5eb7b4f02821c71813ef13ba3d0eccfca127bd1e64d97f077bb3467c31a7808af1bda385230fbbc18db";  // Токен сообщества
+const VK_KEY = "b317fd9b6b4cd0836eb7995c507fdee275b9105942628142d347aa3a8aa2a69d60fb83fe238fcf9cbbd96";  // Токен сообщества
 const ACCESS_KEY = "0ecc4999";  // Тот самый ключ из сообщества 
 const VERSION = "5.81"; // Версия API VK
 
